4207bce464b38929016ed1604f0dc852a6b6a5d9
overmind777 <63595243+overmind777@users.noreply.github.com>
2023-10-29 21:24:52 +0200

    finaly refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,20 +4,11 @@ import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Statistics from './Statistics/Statistics';
 
 export const App = () => {
-  // state = {
-  //   good: 0,
-  //   neutral: 0,
-  //   bad: 0,
-  // };
-
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
   const leaveFeedback = e => {
-    //  setState(prev => ({
-    //   [e]: prev[e] + 1,
-    // }));
     switch (e) {
       case 'good':
         setGood(prev => prev + 1);
